fb89eaaba672b987477d0f98e4bb412fcca50340
dyegosl11 <36959440+dyegosl11@users.noreply.github.com>
2021-05-04 00:54:17 -0300

    Operações Matemáticas

---
+++ Módulo 1/operações_matematicas.py
@@ -0,0 +1,31 @@
+# Tipos de números que podem ser usados em Python
+
+a = 20
+b = 20.5
+print(type(a))
+print(type(b))
+
+print(10 + 6)
+print(10 - 6)
+print(10 * 6)
+print(10 / 6)
+print(10 // 6) # divisão de inteiro
+print(10 % 6)  # modulos
+print(10 ** 6) # exponenciais
+
+# Atalho para atribuição mais rápida
+
+a = 10
+a = a + 10
+a += 5
+
+b = 20
+b = b - 2
+b -= 2
+
+# Operações matemáticas comuns
+# https://docs.python.org/3/library/math.html
+
+print(round(5.7))
+import math
+print(math.ceil(2.2))
