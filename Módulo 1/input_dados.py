82c09415f633c00eeffa7c6664e418dcb9b6ddd4
dyegosl11 <36959440+dyegosl11@users.noreply.github.com>
2021-05-04 00:37:02 -0300

    input de dados

---
+++ Módulo 1/input_dados.py
@@ -0,0 +1,8 @@
+# Recebendo dados do usuário
+
+senha = input('Digite sua senha: ')
+print(senha)
+print(type(senha))
+
+quantidade_filmes = int(input('Quantos filmes você já assistiu este mês? '))
+print(type(quantidade_filmes))
